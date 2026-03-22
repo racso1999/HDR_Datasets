bba6a426e32b2baffe4ca771edd43bee1c2c4c90
OSCARJONES1999 <jones.oscar.work@outlook.com>
2026-03-22 11:37:29 +0000

    first commit

---
+++ app/api/datasets/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server' 
+
+const DATA_URL = 'https://raw.githubusercontent.com/HDRUK/hackathon-entity-linkage/refs/heads/dev/fe-implement/app/data/all_datasets.json'
+
+export async function GET() { // Fetch the dataset information from the provided URL
+    const response = await fetch(DATA_URL) 
+    const data = await response.json() //parse the response as JSON
+
+    const datasets = data.map((dataset: any) => ({ // Extract relevant fields from the dataset and return in structured format
+        title: dataset.metadata.summary.title,
+        description: dataset.metadata.summary.description,
+        accessServiceCategory: dataset.metadata.accessibility.access.accessServiceCategory,
+        accessRights: dataset.metadata.accessibility.access.accessRights,
+    }))
+
+    return NextResponse.json(datasets)
+}
